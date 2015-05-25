43165df04e081baba6ba7a83973ba83230316c63
ideais <ideais@caxambu.ideais.local>
2015-05-25 20:15:03 -0300

    ok

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -3,28 +3,34 @@ package main
 import (
   "fmt"
   "log"
-
+  "strings"
   "github.com/PuerkitoBio/goquery"
 )
 
 func RentScrape() {
-  doc, err := goquery.NewDocument("http://rj.bomnegocio.com/imoveis/aluguel/apartamentos?q=tijuca+-barra&ret=1040&sp=1") 
+  doc, err := goquery.NewDocument("http://rj.olx.com.br/rio-de-janeiro-e-regiao/zona-norte/imoveis/aluguel/apartamentos?ret=1040") 
   if err != nil {
     log.Fatal(err)
   }
 
-  jQuery('.col_2 a').each(function(){ $(this).attr('name') })
-
-  doc.Find(".list_adsBN_item").Each(func(i int, s *goquery.Selection) {
-    id := s.Find(".title mb5px a").Attr('name')
-    name := s.Find(".title mb5px a").Text()
-    link := s.Find(".title mb5px a").Attr('href')
-    description := s.Find(".text mb5px detail_specific").Text()
-    location := s.Find("i").Find('text mb5px detail_region').Text()
-    fmt.Printf("Rent %d: %s - %s\n", i, band, title)
+  doc.Find(".section_OLXad-list ul.list li").Each(func(i int, s *goquery.Selection) {
+    anchor := s.Find("a")
+    id := anchor.AttrOr("name","")[0]
+    name := scrub(anchor.Text())
+    // link := anchor.AttrOr("href","")
+    // description := s.Find(".detail_specific").Text()
+    price := scrub(s.Find(".OLXad-list-price").Text())
+    // location := s.Find("i").Find(".detail_region").Text()
+    fmt.Printf("%v\n", id)
+    fmt.Printf("%v\n", name)
+    fmt.Printf("%v\n", price)
   })
 }
 
 func main() {
   RentScrape()
+}
+
+func scrub(s string) string{
+  return strings.Join( strings.Fields(s), " ") 
 }
