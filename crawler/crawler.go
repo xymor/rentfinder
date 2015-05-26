1f9a7a9e2cfaab790ddee759c43c45358e22b514
xymor <raphaelmiranda@gmail.com>
2015-05-26 00:34:37 -0300

    ok

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -1,29 +1,42 @@
 package main
 
 import (
-  "fmt"
   "log"
+  "fmt"
+  "os"
   "strings"
   "github.com/PuerkitoBio/goquery"
+  "github.com/olivere/elastic"
 )
 
-func RentScrape() {
-  doc, err := goquery.NewDocument("http://rj.olx.com.br/rio-de-janeiro-e-regiao/zona-norte/imoveis/aluguel/apartamentos?ret=1040") 
-  if err != nil {
-    log.Fatal(err)
-  }
+// Create a client and connect to http://192.168.2.10:9201
+var client, err = elastic.NewClient(elastic.SetURL(os.Getenv("ES_URL")))
 
-  doc.Find(".section_OLXad-list ul.list li").Each(func(i int, s *goquery.Selection) {
+
+func RentScrape() {
+
+  doc, errGet := goquery.NewDocument("http://rj.olx.com.br/rio-de-janeiro-e-regiao/zona-norte/imoveis/aluguel/apartamentos?q=tijuca+-barra&ret=1040") 
+  if errGet != nil {
+    fmt.Printf("%v\n", "1")
+    log.Fatal(errGet)
+  } 
+  doc.Find(".section_OLXad-list ul.list li").FilterFunction(func(i int, sel *goquery.Selection) bool{
+    return sel.Find(".col-4 p").First().Text() == "Hoje"
+  }).Each(func(i int, s *goquery.Selection) {
+
     anchor := s.Find("a")
-    id := anchor.AttrOr("name","")[0]
-    name := scrub(anchor.Text())
-    // link := anchor.AttrOr("href","")
-    // description := s.Find(".detail_specific").Text()
-    price := scrub(s.Find(".OLXad-list-price").Text())
-    // location := s.Find("i").Find(".detail_region").Text()
-    fmt.Printf("%v\n", id)
-    fmt.Printf("%v\n", name)
-    fmt.Printf("%v\n", price)
+
+    rent := Rent{Id: anchor.AttrOr("name",""),
+                Name: scrub(anchor.Text()),
+                Price: scrub(s.Find(".OLXad-list-price").Text()),
+                Link: anchor.AttrOr("href","")   ,
+                Description: s.Find(".detail_specific").Text(),
+                Location: s.Find("i").Find(".detail_region").Text()}
+
+    fmt.Printf("%v\n", rent)
+
+    index(rent)
+
   })
 }
 
@@ -33,4 +46,28 @@ func main() {
 
 func scrub(s string) string{
   return strings.Join( strings.Fields(s), " ") 
+}
+
+func index(rent Rent){
+  _, err = client.Index().
+      Index("rent").
+      Type("rent").
+      Id(rent.Id).
+      BodyJson(rent).
+      Do()
+  if err != nil {
+      // Handle error
+      fmt.Printf("%v\n", rent)
+      panic(err)
+  }
+}
+
+type Rent struct {
+    Id     string        `json:"id"`
+    Name  string        `json:"name"`
+    Link string           `json:"link"`
+    Description    string        `json:"description,omitempty"`
+    Location    string        `json:"location,omitempty"`
+    Images    []string         `json:"images,omitempty"`
+    Price    string        `json:"price,omitempty"`
 }
